ff26740f6a1cced0998538bd9c3474eb5aa7236d
Sylwester Sojka <sojka.sylwester.dev@gmail.com>
2025-03-06 21:17:13 +0100

    Added validation of URL

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import pandas as pd
 import streamlit as st
+import re
 from add_player_stats import scrape_player_stats
+
+# Regex do walidacji URL
+url_regex = r'^https:\/\/www\.basketball-reference\.com\/players\/.*$'
 
 # Tytuł aplikacji
 st.title("NBA Player Score Predicter")
 
 # Tworzenie formularza
 with st.form(key='my_form'):
-
     link = st.text_input("Podaj link ze strony www.basketball-reference.com")
-    submit_button = st.form_submit_button("Dodaj do dataframe")
+    submit_button = st.form_submit_button("Sprawdź URL")
 
-# Logika po naciśnięciu przycisku
+# Walidacja URL po naciśnięciu przycisku
 if submit_button:
-    player_df = scrape_player_stats(url=link)
-    st.dataframe(player_df)
-    st.write(f"Statystyk dla gracza: {link} zostaly dodane.")
-
+    if link and not re.match(url_regex, link):
+        st.error("Nieprawidłowy URL. Proszę podać poprawny link.")
+    else:
+        st.success("URL jest poprawny.")
+        player_df = scrape_player_stats(url=link)
+        st.dataframe(player_df)
+        st.write(f"Statystyk dla gracza: {link} zostaly dodane.")
+       
 
